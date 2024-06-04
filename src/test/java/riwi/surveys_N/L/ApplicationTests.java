d2a1bffbcd7f69606700cfc183a6a7fdd74a7174
nicoluna319 <nicolaslunaromero@gmail.com>
2024-06-04 06:29:39 -0500

    first commit

---
+++ src/test/java/riwi/surveys_N/L/ApplicationTests.java
@@ -0,0 +1,13 @@
+package riwi.surveys_N.L;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
